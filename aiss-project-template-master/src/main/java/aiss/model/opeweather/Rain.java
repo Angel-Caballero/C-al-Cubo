7a65642a688d0ac2c3ecc07228530b6ff3c62d29
Angel-Caballero <49127396+Angel-Caballero@users.noreply.github.com>
2019-04-10 20:35:42 +0200

    OpenWeather

---
+++ aiss-project-template-master/src/main/java/aiss/model/opeweather/Rain.java
@@ -0,0 +1,26 @@
+package aiss.model.opeweather;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Rain
+{
+	@JsonProperty("3h")
+    private String last3Hours;
+
+	@JsonProperty("3h")
+    public String getlast3Hours ()
+    {
+        return last3Hours;
+    }
+
+    public void set3h (String last3Hours)
+    {
+        this.last3Hours = last3Hours;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "ClassPojo [3h = "+ last3Hours+"]";
+    }
+}
---
+++ aiss-project-template-master/src/main/java/aiss/model/opeweather/Wind.java
@@ -0,0 +1,34 @@
+package aiss.model.opeweather;
+
+public class Wind
+{
+    private String deg;
+
+    private String speed;
+
+    public String getDeg ()
+    {
+        return deg;
+    }
+
+    public void setDeg (String deg)
+    {
+        this.deg = deg;
+    }
+
+    public String getSpeed ()
+    {
+        return speed;
+    }
+
+    public void setSpeed (String speed)
+    {
+        this.speed = speed;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "ClassPojo [deg = "+deg+", speed = "+speed+"]";
+    }
+}
