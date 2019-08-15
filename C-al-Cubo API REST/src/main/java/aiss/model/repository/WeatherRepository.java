5da66475f29859da7bcf04fc92a7f60da2e9a631
Angel-Caballero <49127396+Angel-Caballero@users.noreply.github.com>
2019-08-15 21:25:07 +0200

    Update WeatherRepository.java

--- C-al-Cubo API REST/src/main/java/aiss/model/repository/WeatherRepository.java
+++ C-al-Cubo API REST/src/main/java/aiss/model/repository/WeatherRepository.java
@@ -13,10 +13,27 @@ public interface WeatherRepository {
 	public Weather getWeather(String weatherId);
 	public void updateWeather(Weather w);
 	public void deleteWeather(String weatherId);
-	public Collection<Playlist> getWeatherPlaylists(String weatherId);
-	public void addPlaylist(String weatherId, String playlistId);
-	public void removePlaylist(String weatherId, String playlistId);
+	public Playlist getWeatherPlaylist(String weatherId);
+	public void setPlaylist(String weatherId, String playlistId);
+	public void removePlaylist(String weatherId);
 	public Collection<Track> getWeatherTracks(String weatherId);
-	public void addSong(String weatherId, String playlistId, String trackId);
-	public void removeSong(String weatherId, String playlistId, String trackId);
+//	public void addSong(String weatherId, String playlistId, String trackId);
+//	public void removeSong(String weatherId, String playlistId, String trackId);
+	
+	//Playlist
+	public void addPlaylist(Playlist p);
+	public Collection<Playlist> getAllPlaylists();
+	public Playlist getPlaylist(String playlistId);
+	public void updatePlaylist(Playlist p);
+	public void deletePlaylist(Playlist p);
+	public Collection<Track> getTracks(String playlistId);
+	public void addTrack(String playlistId, String trackId);
+	public void removeTrack(String playlistId, String trackId);
+	
+	//Track
+	public void addTrack(Track t);
+	public Collection<Track> getAllTracks();
+	public Track getTrack(String trackId);
+	public void updateTrack(Track t);
+	public void deleteTrack(String trackId);
 }
